3f9fdc63a492418846d420539b653f8d51617a3c
Luke <lucas.macd@gmail.com>
2017-05-06 19:15:15 -0400

    Added Homepage with updated routing

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,5 @@
+class GreetingsController < ApplicationController
+  def hello
+  	@message = "Hello to you."
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'greetings/hello'
+
+  root 'greetings#hello' # TODO make this a different landing page
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

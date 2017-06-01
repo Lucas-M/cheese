d45a747630947f75a19a34eddb34f53833b9be37
Luke <lucas.macd@gmail.com>
2017-05-31 22:06:23 -0400

    before user/review relationship

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :users
   resources :reviews
   resources :posts
+  resources :items
+
   get 'greetings/hello'
   get 'greetings/goodbye'
 
   root 'greetings#hello' 
-  resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511164942) do
+ActiveRecord::Schema.define(version: 20170523164735) do
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20170511164942) do
     t.integer  "rating"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
